9bc303d7db786c7ef6fc98d21188022c01900b4a
RaghuVamsiBathula <bathularaghuvamsi3@gmail.com>
2025-01-20 19:43:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Header from './HeaderComponents/Header';
+// import SearchBar from './SearchComponents/Search';
+// import Navigation from './NavigationComponent/Navigation';
+function App() {
+  return (
+  <>
+   <Header />
+  </>
+  );
+}
+
+export default App;
